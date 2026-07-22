37fe5c320423d7f1230ad22299a4bc501582b281
PavaritPramual <pavarit.p@kkumail.com>
2026-07-22 13:49:17 +0700

    feat: initial spring boot project

---
+++ src/main/java/com/example/lab05_bean_scope/Lab05BeanScopeApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab05_bean_scope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab05BeanScopeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab05BeanScopeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lab05_bean_scope/Lab05BeanScopeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab05_bean_scope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab05BeanScopeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
